777e4a186ba95db170fd0ddc3c7fb674679cdfbf
Rudy Ravelin <rudy@noukod.com>
2019-03-03 01:49:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value){
+return Object.assign({}, object, {[key]: value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  var prop = key;
+  object[prop] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  delete object.key;
+  return object;
+}
